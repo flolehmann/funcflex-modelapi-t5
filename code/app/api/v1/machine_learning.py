3b8862ca0e5d611af9cd42e4ce22fffb9a96823f
Florian Lehmann <florian.lehmann@uni-bayreuth.de>
2021-08-26 17:13:17 +0200

    Implemented basic API for GPT-Neo text generation with huggingface

--- code/app/api/v1/machine_learning.py
+++ code/app/api/v1/machine_learning.py
@@ -6,7 +6,7 @@ from starlette import status
 
 from definitions import MODEL_DIR
 
-from transformers import BertTokenizer, EncoderDecoderModel
+from transformers import pipeline
 
 router = APIRouter()
 
@@ -14,23 +14,19 @@ ENTITY = "Machine Learning"
 
 
 # load model only once:
-tokenizer = BertTokenizer.from_pretrained("google/bert2bert_L-24_wmt_de_en", pad_token="<pad>", eos_token="</s>", bos_token="<s>")
-bert2bert = EncoderDecoderModel.from_pretrained("google/bert2bert_L-24_wmt_de_en")
-
+# generator = pipeline('text-generation', model='EleutherAI/gpt-neo-1.3B', device=0)
+generator = pipeline('text-generation', model='EleutherAI/gpt-neo-1.3B')
 
 @router.post("/predict", response_model=schema.prediction.PredictionOutput,
              dependencies=[Depends(methods.api_key_authentication)])
 async def predict(data: schema.prediction.PredictionInput):
     preprocess_text = data.input.strip().replace("\n", "")
 
-    input_ids = tokenizer(preprocess_text, return_tensors="pt", add_special_tokens=False).input_ids
-    output_ids = bert2bert.generate(input_ids)[0]
-
-    output = tokenizer.decode(output_ids, skip_special_tokens=True)
-
+    output = generator(preprocess_text, do_sample=True, min_length=200)
+    result = output[0]["generated_text"].replace("\n", "")
     return {
-        "prediction": output,
-        "function": "translation_de_en"
+        "prediction": result,
+        "function": "generation"
     }
 
 
