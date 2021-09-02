fcfb370f16e2e9b90ced1ab54cfeeaebe30947c2
Florian Lehmann <florian.lehmann@uni-bayreuth.de>
2021-09-02 10:26:28 +0200

    Added cors

--- code/app/main.py
+++ code/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import logging
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.v1 import machine_learning
 
@@ -9,6 +10,20 @@ from api.v1 import machine_learning
 API_PREFIX = "/api/v1"
 
 app = FastAPI(title="study-align-prediction-api", version="1")
+
+origins = [
+    "http://localhost:3000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(
     machine_learning.router,
